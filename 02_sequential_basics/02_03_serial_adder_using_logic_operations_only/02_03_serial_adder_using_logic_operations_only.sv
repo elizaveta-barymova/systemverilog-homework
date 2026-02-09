d10a4828ee82812bc818f3e1ba44330d4d499ba1
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:18:21 +0300

    Update 02_03_serial_adder_using_logic_operations_only.sv

--- 02_sequential_basics/02_03_serial_adder_using_logic_operations_only/02_03_serial_adder_using_logic_operations_only.sv
+++ 02_sequential_basics/02_03_serial_adder_using_logic_operations_only/02_03_serial_adder_using_logic_operations_only.sv
@@ -50,5 +50,16 @@ module serial_adder_using_logic_operations_only
   //
   // See the testbench for the output format ($display task).
 
+  logic carry;
+  wire carry_d;
 
+  assign sum = a ^ b ^ carry;
+  assign carry_d = (a & b) | (a & carry) | (b & carry);
+
+  always_ff @ (posedge clk)
+    if (rst)
+      carry <= '0;
+    else
+      carry <= carry_d;
+  
 endmodule
