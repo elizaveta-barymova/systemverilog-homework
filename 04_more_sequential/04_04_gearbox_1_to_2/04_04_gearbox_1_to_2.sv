9c37dd2cd2d087f2bb4447fd01ae5dc06dbdc73d
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:52:27 +0300

    Update 04_04_gearbox_1_to_2.sv

--- 04_more_sequential/04_04_gearbox_1_to_2/04_04_gearbox_1_to_2.sv
+++ 04_more_sequential/04_04_gearbox_1_to_2/04_04_gearbox_1_to_2.sv
@@ -27,5 +27,24 @@ module gearbox_1_to_2
     // The module should work properly with reset 'rst'
     // and valid 'vld' signals
 
+    logic [width - 1:0] first_part;
+    logic buf_full;
 
+    assign down_data = (buf_full == 1) ? {first_part, up_data} : 'b0;
+    assign down_vld  = buf_full & up_vld;
+        
+    always_ff @ (posedge clk) begin
+        if (rst) begin
+            first_part <=  'b0;
+            buf_full   <= 1'b0;
+        end else begin
+            if (up_vld) begin
+                if (!buf_full) begin
+                    first_part <= up_data;
+                    buf_full   <= 1'b1;
+                end else
+                    buf_full   <= 1'b0;
+            end 
+        end
+    end
 endmodule
