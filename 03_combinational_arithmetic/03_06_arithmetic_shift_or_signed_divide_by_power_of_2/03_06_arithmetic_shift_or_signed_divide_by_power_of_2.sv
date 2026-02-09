15dabe2d7e00529216614a1990a9f440678cc4d8
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:48:31 +0300

    Update 03_06_arithmetic_shift_or_signed_divide_by_power_of_2.sv

--- 03_combinational_arithmetic/03_06_arithmetic_shift_or_signed_divide_by_power_of_2/03_06_arithmetic_shift_or_signed_divide_by_power_of_2.sv
+++ 03_combinational_arithmetic/03_06_arithmetic_shift_or_signed_divide_by_power_of_2/03_06_arithmetic_shift_or_signed_divide_by_power_of_2.sv
@@ -26,7 +26,8 @@ module arithmetic_right_shift_of_N_by_S_using_concatenation
   // concatenations ({a, b}), bit repetitions ({ a { b }}), bit slices
   // and constant expressions.
 
-
+  assign res = {{ S { a[N-1] }}, a[N - 1:S]};
+  
 endmodule
 
 module arithmetic_right_shift_of_N_by_S_using_for_inside_always
@@ -40,7 +41,10 @@ module arithmetic_right_shift_of_N_by_S_using_for_inside_always
   // You are allowed to use only "always_comb" with a "for" loop
   // that iterates through the individual bits of the input.
 
-
+  always_comb begin
+    for (int i = 0; i < N; i ++)
+      res [i] = (i + S < N) ? a[i + S] : a[N-1];
+  end
 endmodule
 
 module arithmetic_right_shift_of_N_by_S_using_for_inside_generate
@@ -51,5 +55,15 @@ module arithmetic_right_shift_of_N_by_S_using_for_inside_generate
   // Implement a module that arithmetically shifts input exactly
   // by `S` bits to the right using "generate" and "for"
 
+  genvar i;
 
+  generate
+    for (i = 0; i < N; i ++)
+      if (i + S < N) begin : shifted_bit_gen
+        assign res [i] = a [i + S];
+      end
+      else begin : first_bit_gen
+        assign res [i] = a[N-1];
+      end
+  endgenerate
 endmodule
