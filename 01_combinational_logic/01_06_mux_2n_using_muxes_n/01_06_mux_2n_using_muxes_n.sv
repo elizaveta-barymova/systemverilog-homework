2d4d044b1f20f74ffca3db6717e881e9661f0e7b
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:33:14 +0300

    Update 01_06_mux_2n_using_muxes_n.sv

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -27,5 +27,27 @@ module mux_4_1
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
 
+  wire [3:0] d01, d23;
+
+  mux_2_1 mux_d01 (
+    .d0(d0), 
+    .d1(d1),
+    .sel(sel[0]),
+    .y(d01)
+  );
+
+  mux_2_1 mux_d23 (
+    .d0(d2), 
+    .d1(d3),
+    .sel(sel[0]),
+    .y(d23)
+  );
+
+  mux_2_1 mux_output (
+    .d0(d01), 
+    .d1(d23),
+    .sel(sel[1]),
+    .y(y)
+  );
 
 endmodule
