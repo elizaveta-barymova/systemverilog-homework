d71a6e3a2b90206e2e22ec08fe42c64f41a9eaba
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:21:21 +0300

    Update 02_06_round_robin_arbiter_with_2_requests.sv

--- 02_sequential_basics/02_06_round_robin_arbiter_with_2_requests/02_06_round_robin_arbiter_with_2_requests.sv
+++ 02_sequential_basics/02_06_round_robin_arbiter_with_2_requests/02_06_round_robin_arbiter_with_2_requests.sv
@@ -23,5 +23,27 @@ module round_robin_arbiter_with_2_requests
     // requests -> 01 00 10 11 11 00 11 00 11 11
     // grants   -> 01 00 10 01 10 00 01 00 10 01
 
+    logic [1:0] prev_grants;
+    logic [1:0] grants_intern;
+
+    always_comb
+    case (requests)
+      2'b00: grants_intern = 'b00;
+      2'b01: grants_intern = 'b01;
+      2'b10: grants_intern = 'b10;
+      2'b11: grants_intern = ~ prev_grants;
+    endcase
+
+    assign grants = grants_intern;
+
+    always_ff @ (posedge clk)
+        if (rst) 
+            prev_grants <= 'b10;
+        else begin
+            if (requests == 'b11) 
+                prev_grants <= grants_intern;
+            else if (requests != 'b00) 
+                prev_grants <= requests;
+        end
 
 endmodule
