092e9b13832f449df763176c1f15e0841c1c9555
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:22:57 +0300

    Update 02_08_parallel_to_serial.sv

--- 02_sequential_basics/02_08_parallel_to_serial/02_08_parallel_to_serial.sv
+++ 02_sequential_basics/02_08_parallel_to_serial/02_08_parallel_to_serial.sv
@@ -29,5 +29,44 @@ module parallel_to_serial
     // Note:
     // Check the waveform diagram in the README for better understanding.
 
+    logic [$clog2(width) - 1:0] counter;
+    logic         [width - 1:0] shift_reg;
+    logic                       processing;
 
+    assign busy = processing;
+
+    always_comb begin
+        if (parallel_valid & ~processing) begin
+            serial_valid = '1;
+            serial_data  = parallel_data[0];
+        end else begin
+            if (processing) begin
+                serial_valid = '1;
+                serial_data  = shift_reg[0];
+            end else begin
+                serial_valid = '0;
+                serial_data  = '0;
+            end
+        end
+    end
+
+    always_ff @ (posedge clk)
+        if (rst) begin
+            counter      <= '0;
+            shift_reg    <= '0;
+            processing   <= '0;
+        end else begin
+            if (parallel_valid & ~processing) begin
+                shift_reg    <= parallel_data[width-1:1];
+                counter      <= width - 2;
+                processing   <= '1;
+            end else if (processing) begin
+                if (counter > 0) begin
+                    shift_reg  <= shift_reg >> 1;
+                    counter    <= counter - 1;
+                end else 
+                    processing <= '0;
+            end
+        end
+    
 endmodule
