3100a4ecd3fde12f35e14859bbf5fd46246e6b83
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:54:20 +0300

    Update 05_01_detect_sequence_using_fsm.sv

--- 05_finite_state_machines/05_01_detect_sequence_using_fsm/05_01_detect_sequence_using_fsm.sv
+++ 05_finite_state_machines/05_01_detect_sequence_using_fsm/05_01_detect_sequence_using_fsm.sv
@@ -75,5 +75,50 @@ module detect_6_bit_sequence_using_fsm
   //
   // Hint: See Lecture 3 for details
 
+  // States
+  enum logic[2:0]
+  {
+     IDLE = 3'b000,
+     S1   = 3'b001,
+     S2   = 3'b010,
+     S3   = 3'b011,
+     S4   = 3'b100,
+     S5   = 3'b101,
+     S6   = 3'b111
+  }
+  state, new_state;
 
+  // State transition logic
+  always_comb begin
+    new_state = state;
+
+    // This lint warning is bogus because we assign the default value above
+    // verilator lint_off CASEINCOMPLETE
+
+    case (state)
+      IDLE: if (  a) new_state = S1;
+      S1:   if (  a) new_state = S2;
+            else     new_state = IDLE;
+      S2:   if (~ a) new_state = S3;
+      S3:   if (~ a) new_state = S4;
+            else     new_state = S1;
+      S4:   if (  a) new_state = S5;
+            else     new_state = IDLE;
+      S5:   if (  a) new_state = S6;
+            else     new_state = IDLE;
+      S6:   if (  a) new_state = S2;
+            else     new_state = S3;
+    endcase
+  end
+
+  // Output logic
+  assign detected = (state == S6);
+
+  // State update
+  always_ff @ (posedge clk)
+    if (rst)
+      state <= IDLE;
+    else
+      state <= new_state;
+      
 endmodule
