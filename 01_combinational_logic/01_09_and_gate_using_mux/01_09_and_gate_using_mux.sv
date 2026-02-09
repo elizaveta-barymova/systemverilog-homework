a9cd4339b58a4f320da45d840631f2e96ad4542a
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:35:28 +0300

    Update 01_09_and_gate_using_mux.sv

--- 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
+++ 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
@@ -28,5 +28,13 @@ module and_gate_using_mux
   // Implement and gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
 
-
+  wire mux_a_out;
+  
+  mux mux_and (
+    .d0(1'b0),
+    .d1(a),
+    .sel(b),
+    .y(o)
+  );
+  
 endmodule
