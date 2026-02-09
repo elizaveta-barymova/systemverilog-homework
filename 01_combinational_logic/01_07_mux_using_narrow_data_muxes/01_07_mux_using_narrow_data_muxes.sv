cc79a7b06bfd7dc0ea0058eb062fbc5a7969ef1a
veta_bary <elizaveta.barymova.00@mail.ru>
2026-02-09 23:33:56 +0300

    Update 01_07_mux_using_narrow_data_muxes.sv

--- 01_combinational_logic/01_07_mux_using_narrow_data_muxes/01_07_mux_using_narrow_data_muxes.sv
+++ 01_combinational_logic/01_07_mux_using_narrow_data_muxes/01_07_mux_using_narrow_data_muxes.sv
@@ -29,5 +29,26 @@ module mux_4_1
   // Implement mux_4_1 with 4-bit data
   // using two instances of mux_4_1_width_2 with 2-bit data
 
+  wire [1:0] y32, y10;
+
+  mux_4_1_width_2 mux_32 (
+    .d0(d0[3:2]),
+    .d1(d1[3:2]),
+    .d2(d2[3:2]),
+    .d3(d3[3:2]),
+    .sel(sel),
+    .y(y32)
+  );
+
+  mux_4_1_width_2 mux_10 (
+    .d0(d0[1:0]),
+    .d1(d1[1:0]),
+    .d2(d2[1:0]),
+    .d3(d3[1:0]),
+    .sel(sel),
+    .y(y10)
+  );
+
+  assign y = {y32, y10};
 
 endmodule
